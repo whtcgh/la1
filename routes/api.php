44d3927a53159608ec153ecdb30e5242eb2491ad
whtcgh <2359092648@qq.com>
2021-06-03 11:05:13 +0800

    CheckParams

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:api')->get('/a', [UserController::class, 'index'] );
+
+
+// Route::get('/user/index', [UserController::class, 'index']);
+
+
+
