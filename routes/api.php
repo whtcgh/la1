6f5b87aca458e667a3f3e1518c94589cb484f861
whtcgh <2359092648@qq.com>
2021-06-09 14:36:30 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,16 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/login', [UserController::class, 'index'] );
 Route::middleware('auth:api')->any('/a', [UserController::class, 'index'] );
+
+// Route::get('login', [UserController::class, 'index'] );
 Route::middleware('auth:api')->any('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
 Route::middleware('auth:api')->any('/user/index', [UserController::class, 'index']);
 // Route::any('/user/index1', [UserController::class, 'index1']);
 // Route::any('/user/test/{id}', [UserController::class, 'test']);
--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::any('/', function () {
     // return view('welcome');
     return 'hello laravel';
 });
-
-Route::any('/wt', [Wt::class, 'index']);
-Route::any('/wts', [Wts::class, 'index']);
-
-
-Route::any('/user/index', [UserController::class, 'index']);
+Route::get('login', 'UserController@index')->name('index');
+// Route::any('/wt', [Wt::class, 'index']);
+// Route::any('/wts', [Wts::class, 'index']);
 
 
-Route::any('/user/test/{id}', [UserController::class, 'test']);
-Route::any('/user/index1', [UserController::class, 'index1']);
+// Route::any('/user/index', [UserController::class, 'index']);
+
+
+// Route::any('/user/test/{id}', [UserController::class, 'test']);
+// Route::any('/user/index1', [UserController::class, 'index1']);
 
 
